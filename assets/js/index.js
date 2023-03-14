b2d846810316eb120383787fc7c64374d9fae48b
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2023-03-14 09:50:03 -0300

    Up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,12 @@
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("nav");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+
 function loading() {
     document.querySelector(".telaLoad").style.display = "none";
     document.querySelector(".container").style.display = "block";
 }
-
